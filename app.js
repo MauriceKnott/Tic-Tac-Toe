3873fde72a728e6c76d9e9bb74a42380eadb97fb
Reese <reeseknott@gmail.com>
2023-05-03 18:09:12 -0700

    First!

--- app.js
+++ app.js
@@ -1,65 +1,275 @@
-const Gameboard = (() => {
-    let gameboard = ["", "", "", "", "", "", "", "", ""]
 
-    const render = () => {
-        let boardHTML = "";
-        gameboard.forEach((square, index) => {
-    const squares = document.querySelectorAll(".field");
-    console.log(squares)
-        })
+ 
 
-    }
-
-
-return {
-    render,
+// player factory function
+const createPlayer = (name, marker) => {
+    return {name, marker};
 }
-})();
-   
-const Game = (() => {
-    let players = [];
-    let currentPlayerIndex;
-    let gameOver;
 
-    const start = () => {
-        players = [
-            // createPlayer(document.querySelector("#player1").value, "X"),
-            // createPlayer(document.querySelector("#player2").value, "O")
-        ]
-        currentPlayerIndex = 0;
-        gameOver = 0;
-        Gameboard.render()
+// gameboard object
+const gameBoard = (() => {
+
+    // generate board array
+    let board = [];
+    for (i = 0; i < 9; i++) {
+        board.push('');
     }
 
-    const handleClick = (event) => {
-        console.log(event.target.id)
-    }
-    
+    // // add event listeners on each square
+    // Array.from(squares.children).forEach((square, index) => {
+    //     square.addEventListener('click', () => {
+    //         // display active player marker
+    //         square.classList.add(game.activePlayer.marker);
+    //         square.setAttribute('data', game.activePlayer.marker);
+    //         // update array value to be that of active player
+    //         board[index] = game.activePlayer.marker;
+    //         // remove event listener from the marked index
+    //         square.style.pointerEvents = 'none';
+    //         // update remainingSpots
+    //         game.remainingSpots -= 1;
+    //         // check winner: if all 3 values within any of these conditions are ===...
+    //         game.checkWinner();
+    //         // check remaining spots
+    //         if (game.winnerDeclared == false) {
+    //             if (game.remainingSpots > 0) {
+    //                 game.alertNextPlayer();
+    //                 game.nextPlayer();
+    //             } else if (game.remainingSpots == 0) {
+    //                 game.declareTie();
+    //             }
+    //         }
+    //     })
+    // });
+
+    // return
     return {
-       start, handleClick
+        board
+    };
+})();
+
+// game object
+const game = (() => {
+
+    // declare players
+    const playerOne = createPlayer('Player 1', 'bolt');
+    const playerTwo = createPlayer('Player 2', 'heart');
+    const mark = document.querySelectorAll(".field");
+
+    // starting point
+    let activePlayer = playerOne;
+    let winnerDeclared = false;
+    let remainingSpots = 9;
+
+    // selectors
+    let subtext = document.querySelector('.subtext'); // display winner/tie
+    let playerName = document.querySelector('.player-name'); // purpose: alert player turn
+
+    // winning conditions
+    const winningAxes = [
+        [0,1,2],
+        [3,4,5],
+        [6,7,8],
+        [0,3,6],
+        [1,4,7],
+        [2,5,8],
+        [0,4,8],
+        [2,4,6],
+    ];
+
+    // check winner
+    function checkWinner() {
+        winningAxes.forEach((item, index) => { // [0, 1, 2, 3, 4, 5, 6, 7]
+            if (gameBoard.board[item[0]] === this.activePlayer.marker && gameBoard.board[item[1]] === this.activePlayer.marker && gameBoard.board[item[2]] === this.activePlayer.marker) {
+                console.log('winner!');
+                subtext.innerHTML = `<b>${this.activePlayer.name} wins!</b>`;
+                this.winnerDeclared = true;
+            } 
+        })
     }
+
+    // alert next player
+    function alertNextPlayer() {
+        this.activePlayer === playerOne ? playerName.textContent = 'Player 2' : playerName.textContent = 'Player 1';
+     
+    }
+
+    // next player
+    function nextPlayer() {
+        this.activePlayer === playerOne ? this.activePlayer = playerTwo : this.activePlayer = playerOne;
+      
+        console.log('nextPlayer() function ran')
+        console.log('active player: ' + activePlayer.name);
+    }
+
+function nextMark() {
+    this.activePlayer === playerOne ? moveBtn.textContent = 'X' : moveBtn.textContent = 'O';
+    console.log(nextMark)
+}
+
+    // declare tie
+    function declareTie() {
+        subtext.innerHTML = "<b>Tie game!</b>";
+    }
+
+    // return
+    return {
+        activePlayer,
+        remainingSpots,
+        checkWinner,
+        alertNextPlayer,
+        nextPlayer,
+        declareTie,
+        winnerDeclared, 
+        nextMark
+    };
 })();
 
 
+const moveBtn = document.querySelectorAll(".field");
 
-    const createPlayer = (name, marker) => {
-        return {name, marker};
-    }
-
-    const moveBtn = document.querySelectorAll(".field");
-
-    moveBtn.forEach((moveBtn) =>{
-        moveBtn.addEventListener('click', Game.handleClick, function() {
-            Game.start();
+    moveBtn.forEach((moveBtn, index) =>{
+        moveBtn.addEventListener('click', function() {
             const btn = document.createElement("p");
-            moveBtn.innerText = 'x';
-            moveBtn.appendChild(btn);
-            console.log(moveBtn)
+       moveBtn.innerText = game.nextMark()
+            console.log(moveBtn.id)
+            gameBoard.board.push(moveBtn.id)
+         console.log(gameBoard.board) 
+         moveBtn.classList.add(game.activePlayer.marker);
+         moveBtn.setAttribute('data', game.activePlayer.marker);
+         // update array value to be that of active player
+         gameBoard.board[index] = game.activePlayer.marker;
+         // remove event listener from the marked index
+         moveBtn.style.pointerEvents = 'none';
+         // update remainingSpots
+         game.remainingSpots -= 1;
+         // check winner: if all 3 values within any of these conditions are ===...
+         game.checkWinner();
+         // check remaining spots
+         if (game.winnerDeclared == false) {
+             if (game.remainingSpots > 0) {
+                 game.alertNextPlayer();
+                 game.nextPlayer();
+             } else if (game.remainingSpots == 0) {
+                 game.declareTie();
+             }
+         }
+         
+        
         });
     
       });
 
+
+// const Gameboard = (() => {
+//     let gameboard = []   //"", "", "", "", "", "", "", "", ""
+
+//     const render = () => {
+//         let boardHTML = "";
+//         gameboard.forEach((square, index) => {
+//     const squares = document.querySelectorAll(".field");
+//     console.log(squares)
+//         })
+
+//     }
+
+//     const update = (index, value) => {
+//         gameboard[index] = value;
+//         render()
+//     }
     
+//     const getGameboard = () => gameboard;
+
+
+// return {
+//     render, gameboard, update, getGameboard
+// }
+// })();
+   
+// const Game = (() => {
+//     let players = [];
+//     let currentPlayerIndex;
+//     let gameOver;
+
+//     const start = () => {
+//         players = [
+//             // createPlayer(document.querySelector("#player1").value, "X"),
+//             // createPlayer(document.querySelector("#player2").value, "O")
+//         ]
+//         currentPlayerIndex = 0;
+//         gameOver = 0;
+//         Gameboard.render()
+//     }
+
+//     const handleClick = (event) => {
+//        let index = parseInt(event.target.id.split("-")[1]);
+//        if (Gameboard.gameboard [index] !== "")
+//        return;
+
+//        Gameboard.update(index, players[cuerrentPlayerIndex].marker);
+//         if (Gameboard.gameboard === checkWinner) {
+//             alert("YOU WIN!")
+//         }
+//     //    if (checkWinner(Gameboard.gameboard(), players[cuerrentPlayerIndex].marker)) {
+//     //     gameOver = true;
+//     //     alert("YOU WIN!")
+//     //    }
+//        currentPlayerIndex = currentPlayerIndex === 0 ? 1 : 0;
+        
+//     }
+//     function checkWinner(cells) {
+//         let combs = [
+//             [0, 1, 2],
+//             [3, 4, 5],
+//             [6, 7, 8],
+//             [0, 3, 6],
+//             [1, 4, 7],
+//             [2, 5, 8],
+//             [0, 4, 8],
+//             [2, 4, 6],
+//         ];
+    
+//         for (let comb of combs) {
+//             if (
+//                 cells[comb[0]].textContent == cells[comb[1]].textContent &&
+//                 cells[comb[1]].textContent == cells[comb[2]].textContent &&
+//                 cells[comb[0]].textContent != ''
+//             ) {
+//                 return true;
+//             }
+//         }
+        
+//         return false;
+//     }
+
+
+    
+//     return {
+//        start, handleClick, checkWinner
+//     }
+// })();
+
+
+
+//     const createPlayer = (name, marker) => {
+//         return {name, marker};
+//     }
+
+//     const moveBtn = document.querySelectorAll(".field");
+
+//     moveBtn.forEach((moveBtn) =>{
+//         moveBtn.addEventListener('click', function() {
+//             const btn = document.createElement("p");
+//             moveBtn.innerText = 'x';
+//             console.log(moveBtn.id)
+//              Gameboard.gameboard.push(moveBtn.id)
+//          console.log( Gameboard.gameboard) 
+//         Game.start.checkWinner
+         
+        
+//         });
+    
+//       });
+
 
 
 
